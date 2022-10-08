9f670648273ae6c8c26b2ad64e9a103800dd34a1
hooks-janayna-cortez <janayna.cortez@hotmail.com>
2022-10-08 15:14:41 -0300

    readme-ok

--- system/system/src/controller/HobbyController.ts
+++ system/system/src/controller/HobbyController.ts
@@ -4,33 +4,33 @@ import { Hobby } from "../database/Classes/Hobby";
 import { HobbyDatabase } from "../database/DataBases/HobbyDataBase";
 
 export class HobbyController{
-    criarHobby =async (req: Request, res: Response): Promise<void> => {
-        let statusCode = 400;
-        try {
-            const { nome } = req.body;
-            if (!nome) {
-              throw new Error("Parâmetros enviados no body não podem ser vazios");
-            }
-            const newId = generateId();
-            const newHobby = new Hobby(newId, nome);
-            const hobbyDB = new HobbyDatabase();
-      await hobbyDB.createHobby(newHobby);
-      res.status(200).send(`Novo hobby ${nome} criada com sucesso`);
-    }catch(error:any){
-        res.status(statusCode).end();
+  criarHobby =async (req: Request, res: Response): Promise<void> => {
+      let statusCode = 400;
+      try {
+          const { nome } = req.body;
+          if (!nome) {
+            throw new Error("Parâmetros enviados no body não podem ser vazios");
+          }
+          const newId = generateId();
+          const newHobby = new Hobby(newId, nome);
+          const hobbyDB = new HobbyDatabase();
+    await hobbyDB.createHobby(newHobby);
+    res.status(200).send(`Novo hobby ${nome} criada com sucesso`);
+  }catch(error:any){
+      res.status(statusCode).end();
 
-    }
+  }
 };
 getHobby = async (req: Request, res: Response): Promise<void> => {
-    let statusCode = 400;
+  let statusCode = 400;
 
-    try {
-      const hobbyDB = new HobbyDatabase();
-      const allHobby = await hobbyDB.getHobby();
-      console.log(allHobby);
-      res.status(200).send(allHobby);
-    } catch (error: any) {
-      res.status(statusCode).end();
-    }
-  };
+  try {
+    const hobbyDB = new HobbyDatabase();
+    const allHobby = await hobbyDB.getHobby();
+    console.log(allHobby);
+    res.status(200).send(allHobby);
+  } catch (error: any) {
+    res.status(statusCode).end();
+  }
+};
 }
